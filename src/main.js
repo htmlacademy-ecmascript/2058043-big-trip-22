41d371ac975aa3acf8bfa0f746041fe36afffd25
admin <ga@usr.by>
2023-12-17 14:11:37 +0300

    Добавлена sort-view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
-
+import './views/add-button-view.js';
+import './views/sort-view.js';
 /**
  * @type {import('./views/brief-view').default}
  */
@@ -11,6 +12,17 @@ const briefView = document.querySelector('brief-view');
  */
 const filterView = document.querySelector('filter-view');
 
+/**
+ * @type {import('./views/add-button-view').default}
+ */
+const addButtonView = document.querySelector('add-button-view');
+
+/**
+ * @type {import('./views/sort-view').default}
+ */
+const sortView = document.querySelector('sort-view');
 
 briefView.render();
 filterView.render();
+addButtonView.render();
+sortView.render();
